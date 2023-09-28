c6172f37d3ec6b7afaa4a4326491dd814bb64a6d
VasiliyMatr <vasiliymatrenin@gmail.com>
2023-09-28 11:59:25 +0300

    [gpr] Add death tests
    - Check out of range assertions failures on invalid read/write

--- gpr/tests/src/test_gpr.cpp
+++ gpr/tests/src/test_gpr.cpp
@@ -1,4 +1,5 @@
 
+#include "gtest/gtest.h"
 #include <random>
 
 #include <gtest/gtest.h>
@@ -10,7 +11,7 @@ namespace sim {
 namespace gpr {
 
 class GPRTest : public ::testing::Test {
-protected:
+  protected:
     static constexpr uint64_t MT_SEED = 1003;
     std::mt19937_64 mt{MT_SEED};
 
@@ -41,6 +42,25 @@ TEST_F(GPRTest, readWrite) {
         ASSERT_EQ(gpr_file.read<uint16_t>(i), static_cast<uint16_t>(value));
         ASSERT_EQ(gpr_file.read<uint8_t>(i), static_cast<uint8_t>(value));
     }
+}
+
+TEST_F(GPRTest, outOfRangeRead) {
+    const char *expected_msg = "Assertion failed: idx < GPR_NUMBER";
+
+    ASSERT_DEATH(std::ignore = gpr_file.read<uint64_t>(GPR_NUMBER),
+                 expected_msg);
+    ASSERT_DEATH(std::ignore = gpr_file.read<uint32_t>(GPR_NUMBER),
+                 expected_msg);
+    ASSERT_DEATH(std::ignore = gpr_file.read<uint16_t>(GPR_NUMBER),
+                 expected_msg);
+    ASSERT_DEATH(std::ignore = gpr_file.read<uint8_t>(GPR_NUMBER),
+                 expected_msg);
+}
+
+TEST_F(GPRTest, outOfRangeWrite) {
+    const char *expected_msg = "Assertion failed: idx < GPR_NUMBER";
+
+    ASSERT_DEATH(gpr_file.write(GPR_NUMBER, mt()), expected_msg);
 }
 
 } // namespace gpr
