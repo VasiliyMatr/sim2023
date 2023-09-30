22cff971d28b3051702ea3b56322afd36b27d536
VasiliyMatr <vasiliymatrenin@gmail.com>
2023-10-01 02:56:35 +0300

    [memory] Refactor memory::PhysMemory
    - Add getters for host ptrs
    - Replace assertions with status returns
    - Refactor and update tests

--- memory/include/sim/memory.hpp
+++ memory/include/sim/memory.hpp
@@ -2,43 +2,99 @@
 #define INCL_MEMORY_HPP
 
 #include <cassert>
-#include <sim/common.hpp>
+#include <cstdint>
 #include <type_traits>
 #include <vector>
+
+#include <sim/common.hpp>
 
 namespace sim {
 namespace memory {
 
-class PhysMemory final {
+struct PhysMemory final {
+    enum class AccessStatus { OK, RANGE_ERROR };
+
+  private:
     std::vector<uint8_t> m_data{};
     PhysAddr m_base_addr = 0;
 
-    template<class UInt>
-    NODISCARD auto calcAndCheckOffset(PhysAddr phys_addr) const noexcept {
-        static_assert(std::is_unsigned_v<UInt>);
-        SIM_ASSERT(phys_addr >= m_base_addr);
+    NODISCARD auto getAccessStatus(PhysAddr phys_addr,
+                                   size_t access_size) const noexcept {
+        if (phys_addr < m_base_addr) {
+            return AccessStatus::RANGE_ERROR;
+        }
 
         auto offset = phys_addr - m_base_addr;
-        SIM_ASSERT(offset + sizeof(UInt) <= m_data.size());
+        if (offset + access_size > m_data.size()) {
+            return AccessStatus::RANGE_ERROR;
+        }
 
-        return offset;
+        return AccessStatus::OK;
     }
 
   public:
-    PhysMemory(PhysAddr base_addr, size_t size) : m_base_addr(base_addr), m_data(size) {}
+    PhysMemory(PhysAddr base_addr, size_t size)
+        : m_base_addr(base_addr), m_data(size) {}
 
     NODISCARD auto baseAddr() const noexcept { return m_base_addr; }
-    template<class UInt>
-    NODISCARD auto read(PhysAddr phys_addr) const noexcept {
+    NODISCARD auto size() const noexcept { return m_data.size(); }
+
+    template <class UInt>
+    NODISCARD AccessStatus read(PhysAddr phys_addr, UInt &dst) const noexcept {
         static_assert(std::is_unsigned_v<UInt>);
-        auto offset = calcAndCheckOffset<UInt>(phys_addr);
-        return *reinterpret_cast<const UInt*>(m_data.data() + offset);
+
+        if (auto status = getAccessStatus(phys_addr, sizeof(UInt));
+            status != AccessStatus::OK) {
+            return status;
+        }
+
+        auto offset = phys_addr - m_base_addr;
+        dst = *reinterpret_cast<const UInt *>(m_data.data() + offset);
+
+        return AccessStatus::OK;
     }
-    template<class UInt>
-    void write(PhysAddr phys_addr, UInt value) {
+
+    template <class UInt>
+    NODISCARD AccessStatus write(PhysAddr phys_addr, UInt value) {
         static_assert(std::is_unsigned_v<UInt>);
-        auto offset = calcAndCheckOffset<UInt>(phys_addr);
-        *reinterpret_cast<UInt*>(m_data.data() + offset) = value;
+
+        if (auto status = getAccessStatus(phys_addr, sizeof(UInt));
+            status != AccessStatus::OK) {
+            return status;
+        }
+
+        auto offset = phys_addr - m_base_addr;
+        *reinterpret_cast<UInt *>(m_data.data() + offset) = value;
+
+        return AccessStatus::OK;
+    }
+
+    NODISCARD AccessStatus
+    getConstHostPtr(PhysAddr phys_addr, size_t access_size,
+                    const uint8_t *&host_ptr) const noexcept {
+        if (auto status = getAccessStatus(phys_addr, access_size);
+            status != AccessStatus::OK) {
+            host_ptr = nullptr;
+            return status;
+        }
+
+        auto offset = phys_addr - m_base_addr;
+        host_ptr = m_data.data() + offset;
+        return AccessStatus::OK;
+    }
+
+    NODISCARD AccessStatus getMuteHostPtr(PhysAddr phys_addr,
+                                          size_t access_size,
+                                          uint8_t *&host_ptr) noexcept {
+        if (auto status = getAccessStatus(phys_addr, access_size);
+            status != AccessStatus::OK) {
+            host_ptr = nullptr;
+            return status;
+        }
+
+        auto offset = phys_addr - m_base_addr;
+        host_ptr = m_data.data() + offset;
+        return AccessStatus::OK;
     }
 };
 
--- memory/tests/src/test_phys_memory.cpp
+++ memory/tests/src/test_phys_memory.cpp
@@ -7,13 +7,16 @@
 
 #include <sim/common.hpp>
 #include <sim/memory.hpp>
+#include <unordered_map>
 
 namespace sim {
 namespace memory {
 
-static constexpr PhysAddr DATA_SEGMENT_BASE_ADDR = 0x5000000000;
-static constexpr size_t SIZE_16MB = size_t{1} << 24;
-static constexpr size_t SIZE_2GB = size_t{1} << 31;
+namespace {
+
+constexpr PhysAddr DATA_SEGMENT_BASE_ADDR = 0x5000000000;
+constexpr size_t SIZE_16MB = size_t{1} << 24;
+constexpr size_t SIZE_2GB = size_t{1} << 31;
 
 class PhysMemoryTest : public ::testing::Test {
   protected:
@@ -22,63 +25,129 @@ class PhysMemoryTest : public ::testing::Test {
     std::mt19937_64 mt{MT_SEED};
 };
 
+} // namespace
+
 TEST_F(PhysMemoryTest, readWrite) {
     PhysMemory pm{DATA_SEGMENT_BASE_ADDR, SIZE_16MB};
 
-    std::vector<PhysAddr> test_phys_addrs = {
-        DATA_SEGMENT_BASE_ADDR, DATA_SEGMENT_BASE_ADDR + 0x0FF5E1,
-        DATA_SEGMENT_BASE_ADDR + SIZE_16MB - sizeof(uint64_t)};
+    std::unordered_map<PhysAddr, uint64_t> test_cases = {
+        {DATA_SEGMENT_BASE_ADDR, mt()},
+        {DATA_SEGMENT_BASE_ADDR + 0x1000, mt()},
+        {DATA_SEGMENT_BASE_ADDR + SIZE_16MB - sizeof(uint64_t), mt()}};
 
-    std::vector<uint64_t> test_values = {mt(), mt(), mt()};
-
-    ASSERT_EQ(test_phys_addrs.size(), test_values.size());
-
-    for (size_t i = 0, end = test_phys_addrs.size(); i != end; ++i) {
-        pm.write(test_phys_addrs[i], uint64_t{test_values[i]});
+    for (auto &test_case : test_cases) {
+        ASSERT_EQ(pm.write(test_case.first, test_case.second),
+                  PhysMemory::AccessStatus::OK);
     }
 
-    for (size_t i = 0, end = test_phys_addrs.size(); i != end; ++i) {
-        PhysAddr pa = test_phys_addrs[i];
-        uint64_t value = test_values[i];
+    for (auto &test_case : test_cases) {
+        PhysAddr pa = test_case.first;
+        uint64_t value = test_case.second;
 
-        ASSERT_EQ(pm.read<uint64_t>(pa), value);
-        ASSERT_EQ(pm.read<uint32_t>(pa), static_cast<uint32_t>(value));
-        ASSERT_EQ(pm.read<uint16_t>(pa), static_cast<uint16_t>(value));
-        ASSERT_EQ(pm.read<uint8_t>(pa), static_cast<uint8_t>(value));
+        uint64_t read_value_64 = 0;
+        ASSERT_EQ(pm.read(pa, read_value_64), PhysMemory::AccessStatus::OK);
+        ASSERT_EQ(read_value_64, value);
+
+        uint32_t read_value_32 = 0;
+        ASSERT_EQ(pm.read(pa, read_value_32), PhysMemory::AccessStatus::OK);
+        ASSERT_EQ(read_value_32, static_cast<uint32_t>(value));
+
+        uint16_t read_value_16 = 0;
+        ASSERT_EQ(pm.read(pa, read_value_16), PhysMemory::AccessStatus::OK);
+        ASSERT_EQ(read_value_16, static_cast<uint16_t>(value));
+
+        uint8_t read_value_8 = 0;
+        ASSERT_EQ(pm.read(pa, read_value_8), PhysMemory::AccessStatus::OK);
+        ASSERT_EQ(read_value_8, static_cast<uint8_t>(value));
     }
 }
 
-TEST(PhysMemory, outOfRange1) {
+namespace {
+
+const std::unordered_map<PhysAddr, const char *> RANGE_ERROR_TEST_CASES = {
+    {0, "Under segment base addr"},
+    {DATA_SEGMENT_BASE_ADDR + SIZE_16MB + 10, "Over segment max addr"},
+    {DATA_SEGMENT_BASE_ADDR + SIZE_16MB - 4, "On segment bound"}};
+
+} // namespace
+
+TEST_F(PhysMemoryTest, rangeErrorReadWrite) {
     PhysMemory pm{DATA_SEGMENT_BASE_ADDR, SIZE_16MB};
 
-    auto matcher =
-        ::testing::HasSubstr("Assertion failed: phys_addr >= m_base_addr");
+    for (const auto &test_case : RANGE_ERROR_TEST_CASES) {
+        const char *descr = test_case.second;
+        PhysAddr pa = test_case.first;
 
-    PhysAddr pa = 0x0;
-    ASSERT_DEATH(std::ignore = pm.read<uint64_t>(pa), matcher);
-    ASSERT_DEATH(pm.write(pa, uint64_t{}), matcher);
+        uint64_t _ = 0;
+
+        ASSERT_EQ(pm.read(pa, _), PhysMemory::AccessStatus::RANGE_ERROR)
+            << descr;
+        ASSERT_EQ(pm.write(pa, uint64_t{}),
+                  PhysMemory::AccessStatus::RANGE_ERROR)
+            << descr;
+    }
 }
 
-TEST(PhysMemory, outOfRange2) {
+TEST_F(PhysMemoryTest, rangeErrorHostPtr) {
     PhysMemory pm{DATA_SEGMENT_BASE_ADDR, SIZE_16MB};
 
-    auto matcher = ::testing::HasSubstr(
-        "Assertion failed: offset + sizeof(UInt) <= m_data.size()");
+    for (const auto &test_case : RANGE_ERROR_TEST_CASES) {
+        const char *descr = test_case.second;
+        PhysAddr pa = test_case.first;
 
-    PhysAddr pa = DATA_SEGMENT_BASE_ADDR + SIZE_16MB + 10;
-    ASSERT_DEATH(std::ignore = pm.read<uint64_t>(pa), matcher);
-    ASSERT_DEATH(pm.write(pa, uint64_t{}), matcher);
+        uint8_t _ = 0;
+
+        const uint8_t *const_host_ptr = &_;
+        ASSERT_EQ(pm.getConstHostPtr(pa, 8, const_host_ptr),
+                  PhysMemory::AccessStatus::RANGE_ERROR)
+            << descr;
+        ASSERT_EQ(const_host_ptr, nullptr) << descr;
+
+        uint8_t *mute_host_ptr = &_;
+        ASSERT_EQ(pm.getMuteHostPtr(pa, 8, mute_host_ptr),
+                  PhysMemory::AccessStatus::RANGE_ERROR)
+            << descr;
+        ASSERT_EQ(mute_host_ptr, nullptr) << descr;
+    }
 }
 
-TEST(PhysMemory, outOfRange3) {
+TEST_F(PhysMemoryTest, constHostAddr) {
     PhysMemory pm{DATA_SEGMENT_BASE_ADDR, SIZE_16MB};
 
-    auto matcher = ::testing::HasSubstr(
-        "Assertion failed: offset + sizeof(UInt) <= m_data.size()");
+    PhysAddr pa = DATA_SEGMENT_BASE_ADDR + 0x1000;
+    const uint8_t *host_ptr = nullptr;
 
-    PhysAddr pa = DATA_SEGMENT_BASE_ADDR + SIZE_16MB - 4;
-    ASSERT_DEATH(std::ignore = pm.read<uint64_t>(pa), matcher);
-    ASSERT_DEATH(pm.write(pa, uint64_t{}), matcher);
+    ASSERT_EQ(pm.getConstHostPtr(pa, 2, host_ptr),
+              PhysMemory::AccessStatus::OK);
+    ASSERT_TRUE(host_ptr != nullptr);
+
+    uint8_t write_value1 = mt();
+    uint8_t write_value2 = mt();
+
+    ASSERT_EQ(pm.write(pa, write_value1), PhysMemory::AccessStatus::OK);
+    ASSERT_EQ(pm.write(pa + 1, write_value2), PhysMemory::AccessStatus::OK);
+    ASSERT_EQ(host_ptr[0], write_value1);
+    ASSERT_EQ(host_ptr[1], write_value2);
+}
+
+TEST_F(PhysMemoryTest, muteHostAddr) {
+    PhysMemory pm{DATA_SEGMENT_BASE_ADDR, SIZE_16MB};
+
+    PhysAddr pa = DATA_SEGMENT_BASE_ADDR + 0x1000;
+    uint8_t *host_ptr = nullptr;
+
+    ASSERT_EQ(pm.getMuteHostPtr(pa, 2, host_ptr), PhysMemory::AccessStatus::OK);
+    ASSERT_TRUE(host_ptr != nullptr);
+
+    host_ptr[0] = mt();
+    host_ptr[1] = mt();
+    uint8_t read_value1 = 0;
+    uint8_t read_value2 = 0;
+
+    ASSERT_EQ(pm.read(pa, read_value1), PhysMemory::AccessStatus::OK);
+    ASSERT_EQ(pm.read(pa + 1, read_value2), PhysMemory::AccessStatus::OK);
+    ASSERT_EQ(read_value1, host_ptr[0]);
+    ASSERT_EQ(read_value2, host_ptr[1]);
 }
 
 } // namespace memory
