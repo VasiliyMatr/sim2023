879d8d2052719bcad3abf59fe0f07b196a86dcb7
Maxim Konoplia <49166705+thelimar@users.noreply.github.com>
2023-11-13 21:40:30 +0300

    [instr][hotfix] Add decoder + hotfix
    * Add initial version of decoder
    * Add tests for decoder
    
    * HOTFIX: Remove deprecated common code
    * HOTFIX: Fix common::bit::signExtend UB on highest bit case
    
    ---------
    
    Co-authored-by: VasiliyMatr <vasiliymatrenin@gmail.com>

--- instr/gen_decode.py
+++ instr/gen_decode.py
@@ -1,14 +1,113 @@
+import yaml
+import subprocess
+import sys
 
-gen = open("decode.gen.cpp", "w")
+KNOWN_REGS = ["rd", "rs1", "rs2", "rm"]
+KNOWN_IMMS = [ "imm12", "imm20", "jimm20", "storeimm", "bimm", "shamt", "shamtw"]
+IMMS_NO_EXTEND = ["shamt", "shamtw"]
 
-gen.write(
-    "#include <sim/instr.hpp>" "\n"
-    "\n"
-    "namespace sim {" "\n"
-    "namespace instr {" "\n"
-    "\n"
-    "Instr::Instr(InstrCode instr_code) {}" "\n"
-    "\n"
-    "}" "\n"
-    "}" "\n"
-)
+def GenerateGetBinValue(bit_section: dict) -> str:
+    write_buffer = ""
+    write_buffer += "bit::getBitField("
+    write_buffer += f"{bit_section.get('msb')},"
+    write_buffer += f"{bit_section.get('lsb')}, instr_code)"
+
+    shift = bit_section.get('to')
+    if shift != 0:
+        write_buffer += f" << {shift}"
+    return write_buffer
+
+
+def GenerateFieldAssigning(inst: dict, field_dict: dict) -> str:
+    write_buffer = ""
+    write_buffer += f"m_id = InstrId::{inst.get('mnemonic').upper().replace('.', '_', 3)};\n"
+    extend_from = 0 
+
+    for field in inst.get("fields"):
+        if field in KNOWN_REGS:
+            write_buffer += f"m_{field} = static_cast<uint8_t>("
+            write_buffer += GenerateGetBinValue(field_dict.get(field).get('location').get('bits')[0])
+            write_buffer += ");\n"
+        elif field in KNOWN_IMMS:
+            for bit_section in field_dict.get(field).get('location').get('bits'):
+                write_buffer += f"m_imm |= static_cast<uint32_t>("
+                extend_from = max(extend_from, bit_section.get('from'))
+                write_buffer += GenerateGetBinValue(bit_section)
+                write_buffer += ");\n"
+
+        if field in KNOWN_IMMS and field not in IMMS_NO_EXTEND and extend_from <= 30:
+            write_buffer += f"m_imm = bit::signExtend({extend_from}, m_imm);\n"
+
+    return write_buffer
+
+
+def GenerateHandleNode(node_dict : dict, field_dict: dict) -> str:
+    write_buffer = ""
+
+    for key, value in node_dict.items():
+
+        if type(key) is int:
+            write_buffer += f"case {key}:\n"
+            if value.get("range") is not None:
+                write_buffer += GenerateHandleNode(value, field_dict)
+            elif value.get("mnemonic") is not None:
+                write_buffer += GenerateFieldAssigning(value, field_dict)
+
+            write_buffer += "break;\n"
+
+        elif key == "range":
+            write_buffer += "switch ("
+            write_buffer += "bit::getBitField("
+            write_buffer += f"{value.get('msb')},"
+            write_buffer += f"{value.get('lsb')}, instr_code)"
+            write_buffer += ")\n"
+
+        elif key == "nodes":
+            write_buffer += "{\n"
+            write_buffer += GenerateHandleNode(value, field_dict)
+            write_buffer += "default:\nbreak;"
+            write_buffer += "}\n"
+
+        else:
+            print("Unknown key:", key)
+
+    return write_buffer
+
+
+def GenerateDecoderTree(yaml_dump: dict) -> str:
+    write_buffer = ""
+    write_buffer += GenerateHandleNode(yaml_dump.get("decodertree"), yaml_dump.get("fields"))
+
+    return write_buffer
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("No risc-v.yaml file provided")
+        return
+    with open(sys.argv[1]) as f:
+        yaml_dump = dict(yaml.safe_load(f))
+
+    write_buffer = "#include <sim/instr.hpp>" "\n"  +\
+        "#include <sim/common.hpp> \n"              +\
+        "#include <iostream> \n"              +\
+        "\n"                                        +\
+        "namespace sim {" "\n"                      +\
+        "namespace instr {" "\n"                    +\
+        "\n"                                        +\
+        "Instr::Instr(InstrCode instr_code) {" "\n"
+
+    write_buffer += GenerateDecoderTree(yaml_dump)
+
+    write_buffer += "}\n"
+    write_buffer += "}" "\n"
+    write_buffer += "}" "\n"
+
+    with open("decode.gen.cpp", "w+") as f:
+        f.write(write_buffer)
+
+    subprocess.run("clang-format -i ./decode.gen.cpp", shell=True)
+
+
+if __name__ == "__main__":
+    main()
--- instr/gen_instr_id.py
+++ instr/gen_instr_id.py
@@ -1,22 +1,42 @@
+import yaml
+import subprocess
+import sys
 
-gen = open("instr_id.gen.hpp", "w")
 
-gen.write(
-    "#ifndef INCL_TYPE_GEN_HPP" "\n"
-    "#define INCL_TYPE_GEN_HPP" "\n"
+def main():
+    if len(sys.argv) < 2:
+        print("No risc-v.yaml file provided")
+        return
 
-    "namespace sim {" "\n"
-    "namespace instr {" "\n"
+    with open(sys.argv[1]) as f:
+        yaml_dump = dict(yaml.safe_load(f))
 
-    "enum class InstrId : uint16_t {" "\n"
-    "    UNDEF," "\n"
-    "    NOP," "\n"
-    "    ADDI," "\n"
-    # ....
-    "};" "\n"
+    write_buffer = "#ifndef INCL_TYPE_GEN_HPP" "\n" +\
+                   "#define INCL_TYPE_GEN_HPP" "\n\n" +\
+                   "#include <cstdint>" "\n\n" +\
+                   "namespace sim {" "\n" +\
+                   "namespace instr {" "\n\n" +\
+                   "enum class InstrId : uint8_t {" "\n"
 
-    "} // namespace instr" "\n"
-    "} // namespace sim" "\n"
+    write_buffer += "UNDEF,\n"
+    for inst in yaml_dump.get("instructions"):
+        inst_name = inst.get("mnemonic").upper()
+        if "." in inst_name:
+            inst_name = inst_name.replace(".", "_", 2)
 
-    "#endif // INCL_TYPE_GEN_HPP" "\n"
-)
+        write_buffer += f"{inst_name},\n"
+
+    write_buffer += "};\n\n"
+    write_buffer += "} // namespace instr" "\n"
+    write_buffer += "} // namespace sim" "\n\n"
+
+    write_buffer += "#endif // INCL_TYPE_GEN_HPP" "\n"
+
+    with open("instr_id.gen.hpp", "w+") as f:
+        f.write(write_buffer)
+
+    subprocess.run("clang-format -i ./instr_id.gen.hpp", shell=True)
+
+
+if __name__ == "__main__":
+    main()
